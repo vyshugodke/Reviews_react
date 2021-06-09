76d7fba27a89b8be5deeaa6dec552e1e46a8e699
vyshugodke <sonavyshu@gmail.com>
2021-06-09 06:11:38 -0700

    commit

---
+++ src/data.js
@@ -0,0 +1,85 @@
+const reviews = [
+    {
+      id: 1,
+      name: 'Vyshu',
+      job: 'web developer',
+      image:
+        'https://cdn.pixabay.com/photo/2015/09/02/12/39/woman-918583_960_720.jpg',
+      text:
+        "Enjoyed reading! Explains in a simple easy way, the process of growing your money, making your money work for you, that it’s not rocket science. A good read anytime, however would highly recommend to those in early twenties or even before",
+    },
+    {
+      id: 2,
+      name: 'Tej',
+      job: 'Data Scientist',
+      image:
+        'https://cdn.pixabay.com/photo/2015/03/03/20/42/man-657869_960_720.jpg',
+      text:
+      'YOU. ARE. THE. DEAD. Oh my God. I got the chills so many times toward the end of this book. It completely blew my mind. It managed to surpass my high expectations and be nothing at all like I expected Or in Newspeak Double Plus Good.Go for it!!'
+    },
+    {
+      id: 3,
+      name: 'Boo',
+      job: 'Actor',
+      image:
+        'https://cdn.pixabay.com/photo/2016/03/27/17/40/man-1283231_960_720.jpg',
+      text:
+        'The book emphasizes on having a deep financial knowledge in becoming rich than having a good initial investment. The author has given many true stroies to put forth his points.',
+    },
+    {
+      id: 4,
+      name: 'Tina',
+      job: 'The boss',
+      image:
+        'https://cdn.pixabay.com/photo/2019/02/11/20/20/woman-3990680_960_720.jpg',
+      text:
+        'I absolutely loved how the author has focused on the things we need to unlearn first and then highlighted aspects that should be inculcated.It is a very helpful guide with a lot of information so I suggest you take it slow',
+    },
+    {
+        id: 5,
+        name: 'Suzie',
+        job: 'CEO',
+        image:
+          'https://cdn.pixabay.com/photo/2016/11/22/06/05/girl-1848454_960_720.jpg',
+        text:
+          'Have been using it for months and it is simply my life saver as i love reading books. Very comfortable to carry during journey and can simply hold with single hand and battery backup makes it best',
+      },
+      {
+        id: 6,
+        name: 'Varma',
+        job: 'Wrestler',
+        image:
+          'https://cdn.pixabay.com/photo/2016/11/29/08/59/man-1868563_960_720.jpg',
+        text:
+          'Very excellent book to understand the personal finance management. The way writer has explained the Income, Expense, Asset and Liabilities is very good. His emphasis is to increase the asset column and reduce the liabilities',
+      },
+      {
+        id: 7,
+        name: 'Rosalina',
+        job: 'Designer',
+        image:
+          'https://cdn.pixabay.com/photo/2017/03/05/23/14/girl-2120196_960_720.jpg',
+        text:
+          'This book is amazing. The only word I can say its so true and definitely makes us realize why we dont have enough money despite earning enough This book will work as a burning torch',
+      },
+      {
+        id: 8,
+        name: 'Mario',
+        job: 'Gamer',
+        image:
+          'https://cdn.pixabay.com/photo/2016/11/29/03/52/man-1867175_960_720.jpg',
+        text:
+          'Best Financial Book I have ever read. The author if this book teaches us the things that the middle class people do not know and how the rich get richer and poor become more poor best Financial book to start with Should go for it ',
+      },
+      {
+        id: 9,
+        name: 'Walugi',
+        job: 'Driver',
+        image:
+          'https://cdn.pixabay.com/photo/2016/11/21/12/42/beard-1845166_960_720.jpg',
+        text:
+          'The books felt more useful when it was teaching the financial basics like assets vs liability or cash flow. Giving it a 5star because it made me think few things I wouldnt have likely thought otherwise and the reading the book served a good purpose for me',
+      },
+  ];
+  
+  export default reviews;
